4906369a4d592aba1cd88a1eeb72493d69e18793
varunram82786 <varunram82786@gmail.com>
2026-07-18 18:57:57 +0530

    file controller

--- ReadMe.md
+++ ReadMe.md
@@ -3,7 +3,25 @@
 # for generating jar file without tests
 ./gradlew clean build -x test
 
+# for running without creating jarfile
+./gradlew bootRun
 
+nohup ./gradlew bootRun > log.txt 2>&1 &
+
+
+---------------------------------------------
     curl -X POST \
-    -F "file=@/home/user/video.mp4" \
+    -F "file=@/sdcard/t1.png" \
     http://192.168.1.8:8090/files/upload
+    
+    curl -X POST \
+    -F "file=@/sdcard/t1.png" \
+    https://raspi-2slp.onrender.com/files/upload
+
+
+    http://localhost:8090/uploads/t1.png
+
+-----------------------------------------------------------
+
+
+
--- src/main/java/com/hawks/raspi/controller/FileController.java
+++ src/main/java/com/hawks/raspi/controller/FileController.java
@@ -1,13 +1,14 @@
 package com.hawks.raspi.controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -41,5 +42,21 @@ public class FileController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(e.getMessage());
         }
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<Resource> getFile(@PathVariable String fileName) throws IOException {
+
+        Path path = Paths.get("uploads").resolve(fileName);
+
+        Resource resource = new UrlResource(path.toUri());
+
+        if (!resource.exists()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(resource);
     }
 }
